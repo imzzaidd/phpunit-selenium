717dae92d68b8e39cf082ccd2043f2c8166b6005
imzzaidd <angieillumi@gmail.com>
2024-05-26 13:14:55 -0600

    login basico con dependencias ya instaladas

---
+++ testing/pages/LoginPage.php
@@ -0,0 +1,51 @@
+<?php
+namespace Testing\Pages;
+
+use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\WebDriverExpectedCondition;
+
+class LoginPage
+{
+    protected $driver;
+
+    public function __construct(RemoteWebDriver $driver)
+    {
+        $this->driver = $driver;
+    }
+
+    public function open()
+    {
+        $this->driver->get('https://practicetestautomation.com/practice-test-login/');
+        $this->waitForElement(WebDriverBy::xpath('//input[@id="username"]'));
+    }
+
+    public function setUsername($username)
+    {
+        $this->driver->findElement(WebDriverBy::xpath("//input[@name='username']"))->sendKeys($username);
+    }
+
+    public function setPassword($password)
+    {
+        $this->driver->findElement(WebDriverBy::xpath("//input[@name='password']"))->sendKeys($password);
+    }
+
+    public function clickLoginButton()
+    {
+        $this->driver->findElement(WebDriverBy::xpath("//button[contains(.,'Submit')]"))->click();
+    }
+
+    public function getSuccessMessage()
+    {
+        $this->waitForElement(WebDriverBy::xpath('//h1[@class="post-title" and contains(text(), "Logged In Successfully")]'));
+        return $this->driver->findElement(WebDriverBy::xpath('//h1[@class="post-title" and contains(text(), "Logged In Successfully")]'))->getText();
+    }
+
+    protected function waitForElement($by, $timeout = 10)
+    {
+        $wait = new \Facebook\WebDriver\WebDriverWait($this->driver, $timeout);
+        $wait->until(WebDriverExpectedCondition::presenceOfElementLocated($by));
+    }
+}
+?>
+
---
+++ testing/tests/LoginTest.php
@@ -0,0 +1,37 @@
+<?php
+namespace Testing\Tests\UI;
+
+use PHPUnit\Framework\TestCase;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Testing\Pages\LoginPage;
+
+class LoginTest extends TestCase
+{
+    protected $driver;
+
+    protected function setUp(): void
+    {
+        $this->driver = RemoteWebDriver::create('http://localhost:4444/wd/hub', DesiredCapabilities::chrome());
+    }
+
+    protected function tearDown(): void
+    {
+        $this->driver->quit();
+    }
+
+    public function testSuccessfulLogin()
+    {
+        $loginPage = new LoginPage($this->driver);
+        $loginPage->open();
+        $loginPage->setUsername('student');
+        $loginPage->setPassword('Password123');
+        $loginPage->clickLoginButton();
+
+        $successMessage = $loginPage->getSuccessMessage();
+        $this->assertStringContainsString('Logged In Successfully', $successMessage);
+    }
+}
+?>
+
+?>
